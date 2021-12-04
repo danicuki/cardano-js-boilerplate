ec13ec48a9690e330e84dc9d24a7ce8c16bb33a2
Daniel Cukier <danicuki@gmail.com>
2021-12-03 22:03:42 -0300

    app working with basic wallet data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import cbor from 'cbor';
 
 function App() {
+
+  const [wallet, setWallet] = useState(false);
+  const [balance, setBalance] = useState(0);
+  const [network, setNetwork] = useState(null);
+  const [cardano, setCardano] = useState(window.cardano);
+
+  // hack when cardano extension take some time to load
+  setTimeout(() => {
+    setCardano(window.cardano);
+  }, 100)
+
+  useEffect(() => {
+    if (!wallet) return;
+    const networks = ['testnet', 'mainnet'];
+
+    const showNetwork = (network) => {
+      setNetwork(networks[network]);
+    }
+        
+    const showWalletData = () => {
+      cardano.getBalance().then(showBalance);
+      cardano.getNetworkId().then(showNetwork);
+    }
+
+    cardano.onNetworkChange((n) => { showWalletData() });
+
+    showWalletData();
+    
+    window.cbor = cbor;
+  }, [wallet, cardano]);
+
+  useEffect(() => {
+    if (cardano) {
+      cardano.isEnabled().then( (enabled) => {
+        if (enabled) {
+          setWallet(true);
+        }
+      });
+    }
+  })
+
+  const showBalance = (string) => {
+    const decoded = cbor.decode(string)
+    if (typeof decoded === 'number') {
+      setBalance(decoded / 1_000_000);
+    } else {
+      setBalance(decoded[0] / 1_000_000)
+    }
+  }
+
+  const connectWallet = () => {
+    cardano.enable().then((resp) => {
+      setWallet(true);
+    }).catch((ex) => {
+      alert(ex.info);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {! window.cardano && (
+          <div>
+            Install <a href="https://namiwallet.io/">Nami Wallet Browser Extension</a> to Test this page
+          </div>
+        )}
+        {window.cardano && (
+          <div>
+            {!(wallet) && (
+              <button onClick={connectWallet}>Connect Wallet</button>        
+            )}
+            {(wallet) && (
+              <div>
+                <h2>Balance: {balance}</h2>
+                <h3>Network: {network}</h3>
+              </div>
+            )}
+          </div>
+        )}
+
+        
       </header>
     </div>
   );
